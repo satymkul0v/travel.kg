4a162ed630a241447a8ee8dc0f523171615cab2b
satymkul0v <danielsatymkulov312@gmail.com>
2025-03-07 14:14:28 +0600

    maain

--- src/Components/Home/AlaToo1/ArslanBap.jsx
+++ src/Components/Home/AlaToo1/ArslanBap.jsx
@@ -8,12 +8,13 @@ function ArslanBap() {
   return (
     <div className='arslan'>
       <h1>ABOUT KYRGYZSTAN</h1>
-
       <div className='storis'>
       <video  className='video1'autoPlay muted loop width='350px' height='400px'>
           <source  src={Video} type="video/mp4" />
           Ваш браузер не поддерживает видео.
         </video>
+
+          <h2>all the beauty of Kyrgyzstan</h2>
         </div>
       </div>
   )
