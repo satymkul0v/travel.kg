e4757a0ab42ae4943c9e18768c8ffd95f95c48af
satymkul0v <danielsatymkulov312@gmail.com>
2025-03-07 15:25:37 +0600

    zvuuk

--- src/Components/Home/AlaToo1/ArslanBap.jsx
+++ src/Components/Home/AlaToo1/ArslanBap.jsx
@@ -9,7 +9,7 @@ function ArslanBap() {
     <div className='arslan'>
       <h1>ABOUT KYRGYZSTAN</h1>
       <div className='storis'>
-      <video  className='video1'autoPlay loop width='350px' height='400px'>
+      <video  className='video1'autoPlay  loop playsInline controls width='350px' height='400px'>
           <source  src={Video} type="video/mp4" />
           Ваш браузер не поддерживает видео.
         </video>
